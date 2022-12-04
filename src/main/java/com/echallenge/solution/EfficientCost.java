3e26f0208a9755ec0d39e3f8bd06bc6f977bd808
Oussama Zbair <oussama.zbair9@gmail.com>
2022-12-04 22:08:18 +0100

    Add .java files

---
+++ src/main/java/com/echallenge/solution/EfficientCost.java
@@ -0,0 +1,51 @@
+package com.echallenge.solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EfficientCost {
+    // not completed in progress...
+
+    /**
+
+     * You will be given an integer array and a threshold value. The threshold represents the
+     * maximum length of subarrays that may be created for the challenge. Each subarray
+     * created has a cost equal to the maximum integer within the subarray. Partition the entire
+     * array into subarrays with lengths less than or equal to the threshold, and do it at a
+     * minimum cost. The subarrays are to be chosen from contiguous elements, and the given
+     * array must remain in its original order.
+     * Example
+     * arr = [1, 3, 4, 5, 2, 6]
+     * threshold = 3
+     * Here are some ways to partition the arrays as an example. The lengths of partitions can
+     * differ as long as none are longer than threshold.
+     * • Partition into 6 subarrays of length 1 as [1], [3], [4], [5], [2], [6]. The total cost is 1+3+4+5+2+6
+     * = 21.
+     * • Partition into 4 subarrays of various lengths: [1, 3], [4], [5], [2, 6]. The total cost is 3 +4 +5 +6 =
+     * 18.
+     * • Partition into 3 subarrays of length 2 as: [1, 3], [4, 5], [2, 6]. The total cost is 3 + 5+ 6 = 14
+     * • Partition into 2 subarrays of length 3 as: [1, 3, 4], [5, 2, 6]. The total cost is 4 + 6 = 10.
+     *
+     *
+     *
+     *
+     *
+     *
+     * The optimal cost is 10.
+     * Function Description
+     * Complete the function calculateCost in the editor below.
+     * calculateCost has the following parameter(s):
+     * int arr[n]: the integer array to be divided into subarrays
+     * int threshold: the threshold value, i.e the maximum size of any subarray
+     * Returns
+     * int: the minimum cost to partition the array
+     * Constraints
+     * 1 ≤n≤5000
+     * 1 ≤ threshold ≤ 500
+     * 1 ≤ arr[i] ≤ 100000
+     * **/
+
+
+
+
+}
---
+++ src/main/java/com/echallenge/solution/PrintSubsequences.java
@@ -0,0 +1,55 @@
+package com.echallenge.solution;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintSubsequences {
+    public static void main(String[] args) {
+        // The input string
+        Scanner inputStr = new Scanner(System.in);
+        String str = inputStr.next();
+
+        // Create an ArrayList to store the subsequences
+        ArrayList<String> subsequences = new ArrayList<>();
+
+        // Add the empty string to the list of subsequences
+        subsequences.add("");
+
+        // Loop through each character of the input string
+        for (int i = 0; i < str.length(); i++) {
+            // Get the size of the current list of subsequences
+            int size = subsequences.size();
+
+            // Loop through each subsequence in the list
+            for (int j = 0; j < size; j++) {
+                // Get the current subsequence
+                String current = subsequences.get(j);
+
+                // Add the current character to the subsequence
+                String updated = current + str.charAt(i);
+
+                // Add the updated subsequence to the list
+                subsequences.add(updated);
+            }
+        }
+
+        // Print all the subsequences
+        for (String subsequence : subsequences) {
+            System.out.println(subsequence);
+        }
+    }
+
+
+    /** This code will print all the subsequences of the string "abc", including the empty string, like this:
+     *
+     * ""
+     * "a"
+     * "ab"
+     * "abc"
+     * "b"
+     * "bc"
+     * "c"
+     */
+
+    // this code I use an ArrayList to store the subsequences.
+}
